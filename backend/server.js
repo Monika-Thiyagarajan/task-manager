65c7d9b8b3a44043e068eee936637740c6e52b77
Monika-Thiyagarajan <monika.anj202@gmail.com>
2025-03-17 00:15:11 +0530

    Server Checking Addition

--- backend/server.js
+++ backend/server.js
@@ -12,10 +12,13 @@ app.use(cors());
 
 connectDB();
 
+app.use('/api', require('./routes/authRoutes'));
 app.use('/api', require('./routes'));
 
-const PORT = process.env.port || 5000;
-app.use('/api',routes);
+app.get('/api/health', (req, res) => {
+    res.json({ message: "API is running!" });
+});
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,()=>{
     console.log(`Server is listening to ${PORT}`);
 })
